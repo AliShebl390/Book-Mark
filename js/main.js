077b5544198745fce38438a639523e0b0d3a4242
Ali Shebl <80364389+AliShebl390@users.noreply.github.com>
2023-12-17 18:17:39 +0200

    adding main files

---
+++ js/main.js
@@ -0,0 +1,46 @@
+var siteName = document.getElementById("siteName");
+var siteLink = document.getElementById("siteLink");
+var sites = []
+if (localStorage.getItem("Dataa") != null) {
+    sites = JSON.parse(localStorage.getItem("Dataa"))
+
+    display()
+}
+function addSite() {
+    var site = {
+        siteName: siteName.value,
+        siteLink: formatLink(siteLink.value)
+    };
+    sites.push(site);
+    display();
+    localStorage.setItem("Dataa", JSON.stringify(sites));
+}
+
+function formatLink(link) {
+    // Check if the link starts with "http://" or "https://"
+    if (!link.startsWith("http://") && !link.startsWith("https://")) {
+        // If not, prepend "https://"
+        link = "https://" + link;
+    }
+    return link;
+}
+
+
+function display() {
+    var output = ``
+    for (var i = 0 ; i < sites.length; i++) {
+        output += `<tr>
+        <td>`+ (i+1) +`</td>
+        <td>`+ sites[i].siteName +`</td>
+        <td> <a href="`+sites[i].siteLink+`" class="btn btn-success px-4">Visit</a> </td>
+        <td> <button onclick="deleteSite(`+i+`)" class="btn btn-danger"> Delete</button> </td>
+    </tr>`
+}
+    document.getElementById("myData").innerHTML = output;
+}
+
+function deleteSite(index) {
+    sites.splice(index,1)
+    display()
+    localStorage.setItem("Dataa",JSON.stringify(sites))
+}
